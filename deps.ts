b0651d3bdb47738e049dad7a51be5a3ad94062f0
Marco Ilari <marilari88@gmail.com>
2020-06-15 22:39:42 +0200

    Aggiornata dipendenza oak alla versione 5.2.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v5.2.0/mod.ts";
 export { MongoClient } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors/mod.ts";
